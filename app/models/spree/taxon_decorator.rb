a445dc27f34f1ccb760be6395b21c88b0e9a0fe5
Miha <miha.kersic@gmail.com>
2013-06-13 00:14:07 +0300

    Update taxon_decorator.rb

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,11 +1,12 @@
-
 Spree::Taxon.class_eval do
   def name_for_select(descendent = nil)
-    if self.parent_id.blank?
-    	descendent ? descendent.name : self.name
-    else
-    	'-- '+self.parent.name_for_select(descendent ? descendent : self)
-    end
+    self.name + " "+ self.permalink
+    
+    #if self.parent_id.blank?
+    #	descendent ? descendent.name : self.name
+    #else
+    #	'- '+self.parent.name_for_select(descendent ? descendent : self)
+    #end
   end
 
 end
