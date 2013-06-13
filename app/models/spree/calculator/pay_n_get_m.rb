b9bd28162c80d64fe8b09d5aa94fea722d3f0ac7
Miha <miha.kersic@gmail.com>
2013-06-13 10:49:27 +0300

    Change pay_n_get_m from group_id to taxon

--- app/models/spree/calculator/pay_n_get_m.rb
+++ app/models/spree/calculator/pay_n_get_m.rb
@@ -2,9 +2,9 @@ module Spree
 	class Calculator::PayNGetM < Calculator
 		preference :to_pay, :integer, :default => 1
 		preference :to_get, :integer, :default => 1
-		preference :product_group_id, :integer, :default => nil
+		preference :taxon, :integer, :default => 0
 
-		attr_accessible :preferred_to_get, :preferred_to_pay, :preferred_product_group_id
+		attr_accessible :preferred_to_get, :preferred_to_pay, :preferred_taxon
 
 		def self.description
 			I18n.t(:pay_n_get_m)
@@ -12,18 +12,18 @@ module Spree
 
 		def compute(object=nil)
 			return 0 if object.nil?
-			return 0 if preferred_to_get.blank? || preferred_to_pay.blank? || preferred_product_group_id.blank?
+			return 0 if preferred_to_get.blank? || preferred_to_pay.blank? || preferred_taxon == 0
 			return 0 if preferred_to_get <= preferred_to_pay
 
 			sum = 0.0
-			product_group = Spree::ProductGroup.find preferred_product_group_id
+			tx = Spree::Taxon.find preferred_taxon
 
 			object.line_items.each do |line_item|
-				if product_group.products.map(&:id).include?(line_item.variant.product_id) 
-					groups = (line_item.quantity / preferred_to_get).to_i
-					discount_per_group = (preferred_to_get - preferred_to_pay) * line_item.price
+				if tx.products.map(&:id).include?(line_item.variant.product_id) 
+					txs = (line_item.quantity / preferred_to_get).to_i
+					discount_per_taxon = (preferred_to_get - preferred_to_pay) * line_item.price
 
-					sum += groups * discount_per_group
+					sum += txs * discount_per_taxon
 				end
 			end
 
